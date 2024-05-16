e89b061ad7e13b14b8b48ff38cb6aafc3664b38a
Rizwan20011 <152096183+Rizwan20011@users.noreply.github.com>
2024-05-16 20:09:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+https://github.com/Rizwan20011/terraform-s3-static-website-hosting.git
+
+
+resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "luffy.png"
+  source = "luffy.png"
+  acl    = "public-read"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+
+  }
+
+  depends_on = [aws_s3_bucket_acl.example]
+}
